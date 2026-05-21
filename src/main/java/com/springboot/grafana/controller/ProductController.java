94cc1362a639c8dd778aba84d08838767e15daac
marcel <marcelewane@yahoo.fr>
2026-05-21 14:45:44 +0200

    added just logs for grafana dashboard

--- src/main/java/com/springboot/grafana/controller/ProductController.java
+++ src/main/java/com/springboot/grafana/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.springboot.grafana.controller;
 import com.springboot.grafana.entity.Product;
 import com.springboot.grafana.model.Post;
 import com.springboot.grafana.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
-
+@Slf4j
 public class ProductController {
 
     private final ProductRepository productRepository;
@@ -26,12 +28,13 @@ public class ProductController {
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
+        log.info("Getting all products from products link");
         return productRepository.findAll();
     }
 
     @GetMapping("/posts")
     public List<Post> getAllPosts() {
-
+        log.info("Getting all posts from posts link");
         ResponseEntity<List<Post>> response =
                 restTemplate.exchange(
                         "https://jsonplaceholder.typicode.com/posts",
@@ -40,6 +43,9 @@ public class ProductController {
                         new ParameterizedTypeReference<List<Post>>() {}
                 );
 
+        if(Objects.requireNonNull(response.getBody()).isEmpty())
+            log.error("No posts found in remote service..");
+        log.info("Get all posts from remote service");
         return response.getBody();
     }
 }
